f382dbdf9750da128d9ceb8321c7d815046f7792
Lakshmi Narayanan <96920159+lakshmi-n-i@users.noreply.github.com>
2024-03-10 19:59:22 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hi, Jenkins\n";
   cout << "Task 5\n";
   cout << "I have successfully built and run\n";
   return 0;
